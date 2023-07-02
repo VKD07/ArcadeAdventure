9fdc8ee0706cc8ec488350ab54ee7f9254b9d002
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-07-02 11:59:56 +0400

    Mini game updated

---
+++ ArcadeAdventure/Assets/Vince/Scripts/DeathHandler2D.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeathHandler2D : MonoBehaviour
+{
+    [SerializeField] float horizontalMinLimit;
+    [SerializeField] float horizMaxLimit;
+    [SerializeField] float verticalMinLimit;
+    [SerializeField] float verticalMaxLimit;
+
+    private void Update()
+    {
+        CheckPlayerPos();
+    }
+
+    private void CheckPlayerPos()
+    {
+        if (transform.localPosition.x > horizontalMinLimit || transform.localPosition.x < horizMaxLimit
+            || transform.localPosition.y < verticalMinLimit || transform.localPosition.y > verticalMaxLimit)
+        {
+            Destroy(gameObject);    
+        }
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Deadly")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
