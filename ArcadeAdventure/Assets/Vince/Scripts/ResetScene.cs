976c0284089719d267efee938aef72ad6ae2c32d
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-07-05 21:42:38 +0400

    Fixed Scene Issue

--- ArcadeAdventure/Assets/Vince/Scripts/ResetScene.cs
+++ ArcadeAdventure/Assets/Vince/Scripts/ResetScene.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ResetScene : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void ResetLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
