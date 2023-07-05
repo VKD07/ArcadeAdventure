88ce58802c136e3870a067800b8af634e30c44bf
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-07-05 21:59:42 +0400

    Fixed Exit Issue

--- ArcadeAdventure/Assets/Scenes/MiniGame Folder/Scripts/pauseMenu.cs
+++ ArcadeAdventure/Assets/Scenes/MiniGame Folder/Scripts/pauseMenu.cs
@@ -16,7 +16,7 @@ public class pauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel"))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (isPaused)
             {
