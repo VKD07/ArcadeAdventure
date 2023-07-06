8d75aa33c087eca2414055c9d02ed3c116a0bf0c
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-07-06 20:26:00 +0400

    Final Push

--- ArcadeAdventure/Assets/Vince/Scripts/GameEvent.cs
+++ ArcadeAdventure/Assets/Vince/Scripts/GameEvent.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu]
 public class GameEvent : ScriptableObject
 {
-
-
     private List<GameEventListener> listeners = new List<GameEventListener>();
 
     public void Raise()
